b6379e459e6306bdf6ac7f6dbed6bac1c59fe89a
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2023-12-04 08:23:08 +0700

    fix: prevent access some page logined

--- client/src/pages/Error/index.js
+++ client/src/pages/Error/index.js
@@ -1,16 +1,18 @@
 import classNames from 'classnames/bind';
+import { useTranslation } from 'react-i18next';
 
 import styles from './Error.module.scss';
 
 const cx = classNames.bind(styles);
 
 function Error() {
+    const { t } = useTranslation('translation', { keyPrefix: 'NotExistedPage' });
     return (
         <div className={cx('wrapper')}>
             <div className={cx('text-blue-900 text-9xl mb-5')}>404</div>
-            <div className={cx('text-gray-900 text-5xl font-black')}>You seem lost</div>
+            <div className={cx('text-gray-900 text-5xl font-black')}>{t('you_seem_lost')}</div>
             <div className={cx('text-gray-900 text-5xl font-black')}>
-                The page you are trying to reach doesn't exist
+                {t('the_page_you_are_trying_to_reach_not_exist')}
             </div>
         </div>
     );
