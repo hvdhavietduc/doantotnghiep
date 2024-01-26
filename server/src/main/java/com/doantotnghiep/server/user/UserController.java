e3e0f7f50b9591f3c116aaf46ae3c2745cb996fe
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-01-26 09:33:44 +0700

    API getAll and delete user

--- server/src/main/java/com/doantotnghiep/server/user/UserController.java
+++ server/src/main/java/com/doantotnghiep/server/user/UserController.java
@@ -3,6 +3,7 @@ package com.doantotnghiep.server.user;
 import com.doantotnghiep.server.config.JwtAuthenticationFilter;
 import com.doantotnghiep.server.config.JwtService;
 import com.doantotnghiep.server.exception.ResponseException;
+import com.doantotnghiep.server.user.Response.AllUserResponse;
 import com.doantotnghiep.server.user.Response.UserResponse;
 import com.doantotnghiep.server.user.dto.UserDto;
 import jakarta.servlet.http.HttpServletRequest;
@@ -29,6 +30,24 @@ public class UserController {
         try {
             User user = jwtService.getUserFromHeader(request);
             return userService.getUserById(user.getId());
+        } catch (ResponseException e) {
+            throw new ResponseException(e.getMessage(), e.getStatus(), e.getStatusCode());
+        }
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<AllUserResponse> getAllUser(@RequestParam(defaultValue = "0") Integer page, @RequestParam(defaultValue = "10") Integer size) throws ResponseException {
+        try {
+            return userService.getAllUser(page, size);
+        } catch (ResponseException e) {
+            throw new ResponseException(e.getMessage(), e.getStatus(), e.getStatusCode());
+        }
+    }
+
+    @DeleteMapping("")
+    public ResponseEntity<Boolean> deleteUser(@RequestParam String id) throws ResponseException {
+        try {
+            return userService.deleteUser(id);
         } catch (ResponseException e) {
             throw new ResponseException(e.getMessage(), e.getStatus(), e.getStatusCode());
         }
--- server/src/main/java/com/doantotnghiep/server/user/UserRepository.java
+++ server/src/main/java/com/doantotnghiep/server/user/UserRepository.java
@@ -1,6 +1,10 @@
 package com.doantotnghiep.server.user;
 
+import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -12,5 +16,9 @@ public interface UserRepository extends MongoRepository<User, String> {
     User findUserByUsername(String username);
     User findUserByEmail(String email);
     User findUsersById(String id);
+    @Nonnull
+    Page<User> findAll(@Nonnull Pageable pageable);
+    long count();
+    void deleteUserById(String id);
 
 }
