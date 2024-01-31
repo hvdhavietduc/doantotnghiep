0d70bbc51aec87e526e1ef5295c51ca925003f86
dat27022002 <dat.nguyen27022002@hcmut.edu.vn>
2024-01-31 23:31:56 +0700

    fear: view detail folder

--- client/src/pages/User/ManageWordFolder/ManageWordFolder.jsx
+++ client/src/pages/User/ManageWordFolder/ManageWordFolder.jsx
@@ -1,18 +1,49 @@
-import { useLocation } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Loading from '~/components/Loading';
 import styles from './ManageWordFolder.module.scss';
 import classNames from 'classnames/bind';
 
 import { useTranslation } from 'react-i18next';
-import notify from '~/utils/notify';
+import Button from '~/components/Button';
+import ItemWord from './ItemWord';
+import Pagination from '~/components/Pagination';
 
 const cx = classNames.bind(styles);
 
 function ManageWordFolder() {
     const [loading, setLoading] = useState(false);
 
-    return <div className={cx('')}>Hello{loading && <Loading />}</div>;
+    const { t } = useTranslation('translation', { keyPrefix: 'ManageWordFolder' });
+
+    return (
+        <div className={cx('ManageWordFolder')}>
+            <div className={cx('inner')}>
+                <div className={cx('header')}>
+                    <div className={cx('nameFolder')}>
+                        Folder: <span>Communicate</span>
+                    </div>
+                    <Button primary className={cx('btn-add')}>
+                        {t('add_new_word')}
+                    </Button>
+                </div>
+                <div className={cx('content')}>
+                    {/* <div className={cx('not-have-word')}>There are no vocabulary words in this folder yet</div> */}
+                    <div className={cx('have-word')}>
+                        <div className={cx('header-content')}></div>
+                        <div className={cx('list-word')}>
+                            <ItemWord />
+                            <ItemWord />
+                            <ItemWord />
+                        </div>
+                    </div>
+                </div>
+                <div className={cx('pagination')}>
+                    <Pagination totalPage={10} currentPage={1} onPageChange={() => {}} />
+                </div>
+            </div>
+            {loading && <Loading />}
+        </div>
+    );
 }
 
 export default ManageWordFolder;
